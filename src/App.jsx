37e6b4b706b8f72ff3888fc2a7be3c77059ce4c9
MohaSetif <BouroubaMohamed004@gmail.com>
2025-02-26 11:33:05 +0100

    Adding Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard, Auth } from "@/layouts";
 
 function App() {
   return (
-    <>
-      <p className='text-red-500'>Hello</p>
-    </>
-  )
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
